aeabf4a355129e12dea8a44f3df86f6aad70e9c3
Haqiim <hqcoder.jh@gmail.com>
2024-03-30 16:38:02 +0100

    #

--- scripts/script.js
+++ scripts/script.js
@@ -140,19 +140,19 @@ function changePage(){
     })
 }
 
-// window.onresize = () =>{
-//     if(innerWidth >= 1000){
-//             navbtn.forEach(btn=>{
-//             aside.append(btn)
-//         })
-//         playerpage.style.display = 'flex'
-//         aside2.append(playerpage)
-//         navigation.style.display="none"
-//     }else{
-//         playerpage.style.display = "none"
-//         main.append(playerpage)
-//     }
-// }
+window.onresize = () =>{
+    if(innerWidth >= 1000){
+            navbtn.forEach(btn=>{
+            aside.append(btn)
+        })
+        playerpage.style.display = 'flex'
+        aside2.append(playerpage)
+        navigation.style.display="none"
+    }else{
+        playerpage.style.display = "none"
+        main.append(playerpage)
+    }
+}
 
 
 
